703c3cf84fcc25008ae3cb1bf3c8e0a16139217a
pratik <pratikkayastha98@gmail.com>
2016-10-26 00:07:45 +0530

    Change UI of Reset and PasteUrl Button and when you click on contribute in about tab that opens your github project link.

--- app/src/main/java/com/yellowsoft/subhankar/downinsta/AboutActivity.java
+++ app/src/main/java/com/yellowsoft/subhankar/downinsta/AboutActivity.java
@@ -1,18 +1,39 @@
 package com.yellowsoft.subhankar.downinsta;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
 
+    Button contribute;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        contribute  = (Button) findViewById(R.id.button);
+        contribute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/raj-subhankar/DownInsta"));
+                    startActivity(browserIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(AboutActivity.this, "No application can handle this request."
+                            + " Please install a webbrowser", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
